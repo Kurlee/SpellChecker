e66e01980b3afe8fece1cb76f790b388af093e4d
Adrian Hassan Abdala <aha392@nyu.edu>
2019-09-04 14:47:47 -0400

    developing dictionary laod function

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include "spell.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
--- spell.c
+++ spell.c
@@ -6,9 +6,12 @@
 //  Copyright © 2019 Adrian Abdala. All rights reserved.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "spell.h"
 #include "AppSecAssignment1/dictionary.h"
 
+    char word[123132][26];
 int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]);
 
 /**
@@ -19,5 +22,28 @@ bool check_word(const char* word, hashmap_t hashtable[]);
 /**
  * Loads dictionary into memory.  Returns true if successful else false.
  */
-bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]);
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
+{
+    FILE * dict = NULL;
+    int i = 0;
+    ssize_t read;
+    size_t * len = 0;
+    
+    int max_word_length = 26;
+    int line_buffer = 100;
+    char ** words = (char **)malloc(sizeof(char *)*line_buffer);
+    if (words == NULL)
+    {
+        fprintf( stderr, "Out of memory(1)\n" );
+        exit(1);
+    }
+    if ((dict = fopen(dictionary_file, "r")) == NULL)
+    {
+        fprintf( stderr, "Error opening file");
+        exit(2);
+    }
+
+    fclose(dict);
+    
+}
 

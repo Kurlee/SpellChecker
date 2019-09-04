64deb92b35c83df6e40b729a89285487023d4304
Adrian Hassan Abdala <aha392@nyu.edu>
2019-09-04 11:57:18 -0400

    init

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SpellChecker
+//
+//  Created by admin on 9/4/19.
+//  Copyright © 2019 Adrian Abdala. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

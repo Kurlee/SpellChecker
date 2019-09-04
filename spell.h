97225d422b993004e195006732963417e0da92ad
Adrian Hassan Abdala <aha392@nyu.edu>
2019-09-04 12:30:06 -0400

    init

---
+++ spell.h
@@ -0,0 +1,14 @@
+//
+//  spell.h
+//  SpellChecker
+//
+//  Created by admin on 9/4/19.
+//  Copyright © 2019 Adrian Abdala. All rights reserved.
+//
+
+#ifndef spell_h
+#define spell_h
+
+#include <stdio.h>
+
+#endif /* spell_h */

4230cda5a6909bd5afdf6fd5d2c8f4d8a53b7a78
Adrian Hassan Abdala <aha392@nyu.edu>
2019-09-06 16:15:23 -0400

    testing with print passing, moving to implementation of linked list.

--- spell.c
+++ spell.c
@@ -16,36 +16,49 @@
 #define MAX_WORDS_IN_DICTIONARY  124000
 
 
-int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]);
+//int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]);
 
 /**
  * Returns true if word is in dictionary else false.
  */
-bool check_word(const char* word, hashmap_t hashtable[]);
+//bool check_word(const char* word, hashmap_t hashtable[]);
 
 /**
  * Loads dictionary into memory.  Returns true if successful else false.
  */
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
 {
-    FILE * dictionary_list;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    int i = 0;
+    FILE *dictionary_list;
+    char *line_buff = NULL;
+    int line_count = 0;
+    size_t line_buff_size = 0;
+    ssize_t line_size;
+    node root;
+    node link;
+    
 
     if ((dictionary_list = fopen(dictionary_file, "r")) == NULL)
     {
-        fprintf(stderr, " Error opening file");
+        fprintf(stderr, " Error opening file\n");
         exit(1);
     }
     
-    while ((read = getline(&line, &len, dictionary_list)) != -1)
+    line_size = getline(&line_buff, &line_buff_size, dictionary_list);
+    
+    while (line_size >= 0)
     {
-        printf("word: %s\n", line);
-       // hashtable[i] = hash_function(line);
-        i++;
+        printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_count, line_size, line_buff_size, line_buff);
+        strcpy(root.word, line_buff);
+        root.next = &link;
+        hashtable[line_count] = &root;
+        line_count = line_count + 1;
+
+        line_size = getline(&line_buff, &line_buff_size, dictionary_list);
+        
     }
+    free(line_buff);
+    line_buff = NULL;
+    
 
     fclose(dictionary_list);
     return true;

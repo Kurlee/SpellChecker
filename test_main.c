0388888539b9d4ea61e335aef6c44a4423376f0d
Adrian Hassan Abdala <aha392@nyu.edu>
2019-09-22 11:24:47 +0000

    Update test_main.c
    fix

--- test_main.c
+++ test_main.c
@@ -11,7 +11,6 @@ START_TEST(test_dictionary_normal)
 {
     hashmap_t hashtable[HASH_SIZE];
     ck_assert(load_dictionary(TESTDICT, hashtable));
-    ch_assert()
     // Here we can test if certain words ended up in certain buckets
     // to ensure that our load_dictionary works as intended. I leave
     // this as an exercise.
